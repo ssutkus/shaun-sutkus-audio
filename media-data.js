ef9ce6858a31e4e0fc593e9d67051abc0c5a9e13
ssutkus <ssutkuscras@gmail.com>
2026-07-26 11:10:54 -0400

    Update media-data.js for approved site revision

--- media-data.js
+++ media-data.js
@@ -6,8 +6,8 @@
  *
  * Bandcamp album ID:
  *   Copy the number after album= in Bandcamp's Share / Embed code.
- * Optional sixth value:
- *   Add a Bandcamp track ID to make that song open first.
+ * Bandcamp track ID:
+ *   The sixth value is required so playback stops after the selected song.
  *
  * Reorder complete lines to change playback order.
  * Keep the quotation marks and commas in place.
@@ -29,29 +29,29 @@ window.SHAUN_MEDIA = {
     ["1205052735", "Empath", "Roses That Cry", "Recorded & mixed", "https://f4.bcbits.com/img/a3876503034_5.jpg", "1009731782"],
     ["2053727309", "Amen Dunes", "Freedom (Live in Vienna)", "FOH mix", "https://f4.bcbits.com/img/a2824894190_5.jpg", "4113155612"],
     ["1335211936", "Perfect Pussy", "Driver", "Engineered & produced", "https://f4.bcbits.com/img/a2728979343_5.jpg", "3133141792"],
-    ["1463204543", "Ben Bondy", "°oje", "Mastered", "https://f4.bcbits.com/img/a0886990583_5.jpg"],
-    ["2045080878", "SIGNAL", "LATEX LOVE", "Recorded, mixed & mastered", "https://f4.bcbits.com/img/a2812631845_5.jpg"],
+    ["1463204543", "Ben Bondy", "°oje", "Mastered", "https://f4.bcbits.com/img/a0886990583_5.jpg", "2193914706"],
+    ["2045080878", "SIGNAL", "LATEX LOVE", "Recorded, mixed & mastered", "https://f4.bcbits.com/img/a2812631845_5.jpg", "4090880569"],
     ["2053727309", "Amen Dunes", "Skipping School (Live in Vienna)", "FOH mix", "https://f4.bcbits.com/img/a2824894190_5.jpg", "2918603732"],
     ["729748445", "Bella’s Bartok", "Fiddler and the Devil", "Engineered", "https://f4.bcbits.com/img/a1076244558_5.jpg", "1092741468"],
     ["580794916", "Green Dreams", "Poor Man", "Recorded & mixed", "https://f4.bcbits.com/img/a3070401058_5.jpg", "83199011"],
-    ["2602447278", "SSWAMPZZ", "DRAIN THE SWAMP", "Mixed with Ray McAndrew, Ricardo Balmaseda & Garrett Koloski", "https://f4.bcbits.com/img/a4090079709_5.jpg"],
-    ["3107398266", "The Bird Calls", "My Life in Hollywood", "Mastered", "https://f4.bcbits.com/img/a0995530233_5.jpg"],
+    ["2602447278", "SSWAMPZZ", "What’s Happening To My Body", "Mixed with Ray McAndrew, Ricardo Balmaseda & Garrett Koloski", "https://f4.bcbits.com/img/a4090079709_5.jpg", "775200575"],
+    ["3107398266", "The Bird Calls", "My Life in Hollywood", "Mastered", "https://f4.bcbits.com/img/a0995530233_5.jpg", "2297308757"],
     ["2555058390", "Topical Solution", "one", "Band member", "https://f4.bcbits.com/img/a3589607938_5.jpg", "2265908704"],
     ["1335211936", "Perfect Pussy", "Interference Fits", "Engineered & produced", "https://f4.bcbits.com/img/a2728979343_5.jpg", "2453337044"],
-    ["4064234388", "vitimin", "i", "Written, produced & mixed with Ben Bondy", "https://f4.bcbits.com/img/a0378632687_5.jpg"],
+    ["4064234388", "vitimin", "arte", "Written, produced & mixed with Ben Bondy", "https://f4.bcbits.com/img/a0378632687_5.jpg", "2435783661"],
     ["2053727309", "Amen Dunes", "Miki Dora (Live in Prague)", "FOH mix", "https://f4.bcbits.com/img/a2824894190_5.jpg", "214710306"],
     ["2301953075", "Birth (Defects)", "Youth", "Recorded, mixed & mastered", "https://f4.bcbits.com/img/a4115898148_5.jpg", "1325400025"],
-    ["3782180778", "Friendless Bummer", "Militia EP", "Mixed & mastered", "https://f4.bcbits.com/img/a0973187969_5.jpg"],
+    ["3782180778", "Friendless Bummer", "Militia", "Mixed & mastered", "https://f4.bcbits.com/img/a0973187969_5.jpg", "1793183651"],
     ["1343589502", "Panoram", "Healing Codes", "Co-written with Panoram", "https://f4.bcbits.com/img/a1130556529_5.jpg", "3383067861"],
-    ["279853432", "Pretengineer", "Responsible Fall", "Artist / producer as Pretengineer", "https://f4.bcbits.com/img/a2732658135_5.jpg"],
-    ["2988895360", "#1 Kid", "Boytones", "Engineered with Ian Hersey", "https://f4.bcbits.com/img/a1918236112_5.jpg"],
+    ["279853432", "Pretengineer", "Responsible Fall", "Artist / producer as Pretengineer", "https://f4.bcbits.com/img/a2732658135_5.jpg", "3250185893"],
+    ["2988895360", "#1 Kid", "Night Ranger", "Engineered with Ian Hersey", "https://f4.bcbits.com/img/a1918236112_5.jpg", "3819628788"],
     ["2053727309", "Amen Dunes", "LA (Live in Brighton)", "FOH mix", "https://f4.bcbits.com/img/a2824894190_5.jpg", "1652176524"],
-    ["3350028322", "Eraser", "Hideout", "Recorded & mixed", "https://f4.bcbits.com/img/a2788402533_5.jpg"],
-    ["432371174", "Empath", "Environments", "Recorded with Daisy Ulrich", "https://f4.bcbits.com/img/a3056886438_5.jpg"],
-    ["545226951", "Alexander F", "Alexander F", "Noise, springs & tapes", "https://f4.bcbits.com/img/a1481115975_5.jpg"],
-    ["2880865416", "Education Reform", "The Push-Back Is Missing", "Engineered with Ian Hersey", "https://f4.bcbits.com/img/a2483866116_5.jpg"],
-    ["2814024726", "Know Nothing", "III", "Recorded & mixed; engineering & tape manipulation", "https://f4.bcbits.com/img/a4158813676_5.jpg"],
-    ["4265107355", "The Bird Calls", "Old Faithful", "Mastered", "https://f4.bcbits.com/img/a1888695810_5.jpg"],
-    ["2373529333", "Know Nothing", "Live at Gorham Bros Music 12/22/12", "Mastered", "https://f4.bcbits.com/img/a1238781359_5.jpg"]
+    ["3350028322", "Eraser", "Paints Peeling", "Recorded & mixed", "https://f4.bcbits.com/img/a2788402533_5.jpg", "836511852"],
+    ["432371174", "Empath", "Polyfoam", "Recorded with Daisy Ulrich", "https://f4.bcbits.com/img/a3056886438_5.jpg", "3312622181"],
+    ["545226951", "Alexander F", "Call Me Pretty", "Noise, springs & tapes", "https://f4.bcbits.com/img/a1481115975_5.jpg", "1234906259"],
+    ["2880865416", "Education Reform", "Listen to the Multiplication", "Engineered with Ian Hersey", "https://f4.bcbits.com/img/a2483866116_5.jpg", "1864779622"],
+    ["2814024726", "Know Nothing", "Primal Baggage", "Recorded & mixed; engineering & tape manipulation", "https://f4.bcbits.com/img/a4158813676_5.jpg", "902027446"],
+    ["4265107355", "The Bird Calls", "Old Faithful", "Mastered", "https://f4.bcbits.com/img/a1888695810_5.jpg", "2241672127"],
+    ["2373529333", "Know Nothing", "Home (Live at Gorham Bros Music)", "Mastered", "https://f4.bcbits.com/img/a1238781359_5.jpg", "2962167245"]
   ]
 };
